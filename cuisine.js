7bcf23c452fb59202d6b28bf85d1eb0c08432407
The-olajumoke <62016089+The-olajumoke@users.noreply.github.com>
2020-07-28 16:22:06 +0100

    Add files via upload

---
+++ cuisine.js
@@ -0,0 +1,23 @@
+
+function openNav(){
+    document.getElementById("main").style.marginRight="20%";
+
+    document.getElementById("mySidenav").style.width="30%";
+
+
+}
+function closeNav(){
+    document.getElementById("mySidenav").style.width="0";
+    document.getElementById("main").style.marginRight="0";
+    document.body.style.backgroundColor="white";
+}
+
+
+// var sect = document.addEventListener('click', closeSideNav);
+// function closeSideNav(){
+
+//     document.getElementById("mySidenav").style.width="0";
+//     document.getElementById("main").style.marginRight="0";
+//     document.body.style.backgroundColor="white";
+    // console.log('here')
+// }
